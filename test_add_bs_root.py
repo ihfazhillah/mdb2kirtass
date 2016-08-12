354ac3ad66f783ccd2ca63b5219ff9118bd0d8f8
ihfazhillah <github.com>
2016-08-12 21:37:24 +0700

    menambahkan test kalau yang ditambah sudah ada .. maka... diubah idnya...

--- test_add_bs_root.py
+++ test_add_bs_root.py
@@ -6,7 +6,8 @@ from add_root import add_root
 
 class AddRootBsTestCase(unittest.TestCase):
 
-    original_group_string = """
+    def original_group_string (self):
+        return """
         <setting>
             <root Name='abc' id='1'>
                 <Item Name='def' id='1'>
@@ -16,9 +17,22 @@ class AddRootBsTestCase(unittest.TestCase):
         </setting>
         """.strip()
 
+
+
     def test_add_root(self):
-        changed = add_root(StringIO(self.original_group_string), {'name':'new', 'id':'123'})
+        changed = add_root(StringIO(self.original_group_string()), {'name':'new',
+                                                                    'id':'123'})
         self.assertIn(b'new', etree.tostring(changed))
+
+    def test_add_root_twice(self):
+        changed = add_root(StringIO(self.original_group_string()),
+                 {'name':'abc', 'id':'1'})
+
+        self.assertIn(b'abc', etree.tostring(changed))
+        self.assertIn(b'1_1', etree.tostring(changed))
+
+
+
 
 
 if __name__ == '__main__':
