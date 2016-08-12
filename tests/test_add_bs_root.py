a9477a875530cb4e2c9277011167781bc751dcab
ihfazhillah <github.com>
2016-08-12 23:52:10 +0700

    pindah yang tadi di delet ke situ

---
+++ tests/test_add_bs_root.py
@@ -0,0 +1,38 @@
+import unittest
+from lxml import etree
+from io import StringIO
+
+from add_root import add_root
+
+class AddRootBsTestCase(unittest.TestCase):
+
+    def original_group_string (self):
+        return """
+        <setting>
+            <root Name='abc' id='1'>
+                <Item Name='def' id='1'>
+                    <bk />
+                </Item>
+            </root>
+        </setting>
+        """.strip()
+
+
+
+    def test_add_root(self):
+        changed = add_root(StringIO(self.original_group_string()), 'new')
+        self.assertIn(b'new', etree.tostring(changed))
+        self.assertIn(b'0', etree.tostring(changed))
+
+    def test_add_root_twice(self):
+        changed = add_root(StringIO(self.original_group_string()), 'abc')
+
+        self.assertIn(b'abc', etree.tostring(changed))
+        self.assertIn(b'2', etree.tostring(changed))
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
