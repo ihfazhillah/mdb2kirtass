c36d0ca0b795a3954f2b1ef04deacc21892a5a75
ihfazhillah <github.com>
2016-08-13 06:02:20 +0700

    menambah proses prefix

--- mdb2kirtass/add_root.py
+++ mdb2kirtass/add_root.py
@@ -1,7 +1,7 @@
 from lxml import etree
 
 
-def add_root(original, name):
+def add_root(original, name, id_prefix=None):
     """Menambahkan root dari xml objek berupa original, dengan
     data name_id yang berupa dictionary objek
         name = nama
@@ -11,12 +11,18 @@ def add_root(original, name):
     >> add_root(original, {'name':'shamela_book', 'id':'bs1'})
     """
 
+    if id_prefix is not None:
+        id_ = id_prefix
+    else:
+        id_ = ""
+
     tree = etree.parse(original)
     setting = tree.getroot()
     root = root = tree.find('.//root[@Name="{}"]'.format(name))
     if root is not None:
         root_len = len(root)
-        id_ = root_len + 1
+        id_ = id_ + str(root_len + 1)
+
         new = etree.SubElement(setting, 'root', {'name': name,
                                                 'id': str(id_)})
     else:
