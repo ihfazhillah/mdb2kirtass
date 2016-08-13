7ba13f132ffbd3f08a5695fdc9d2a4cfd3a0a9df
ihfazhillah <github.com>
2016-08-13 17:49:23 +0700

    memperbaiki format original_csv_file

--- tests/test_csv_to_xml.py
+++ tests/test_csv_to_xml.py
@@ -8,14 +8,14 @@ from mdb2kirtass.csv_to_xml import CsvtoXml
 class CsvtoXmlTestCase(unittest.TestCase):
 
     def original_csv_file(self):
-        return StringIO("""bk, no, betaka, authno, cat
-        nama, 1, ini buku bagus, 1, 3
-        munawir, 2, kamus indo arab, 2, 4
-        sidu, 3, kertas putih, 4, 3""")
+        return StringIO("""bk,no,betaka,authno,cat
+nama,1,ini buku bagus,1,3
+munawir,2,kamus indo arab,2,4
+sidu,3,kertas putih,4,3""".strip())
 
 
     def test_dapatkan_satu_baris_pertama(self):
         data = dict(bk='nama', no='1',
                     betaka='ini buku bagus', authno='1', cat='3')
         hasil = CsvtoXml(self.original_csv_file())
-        self.assertEqual(data, hasil._dict_csv()[0])
+        self.assertEqual(data, list(hasil._dict_csv())[0])
