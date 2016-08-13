6c6c2a454f24f77fcec0a28363ab3f15ffcdac0f
ihfazhillah <github.com>
2016-08-13 17:30:00 +0700

    menambahkan original_csv_file fungsi untuk seakan file csv yang akan diproses nantinya

--- tests/test_csv_to_xml.py
+++ tests/test_csv_to_xml.py
@@ -4,4 +4,11 @@ from io import StringIO
 
 
 class CsvtoXmlTestCase(unittest.TestCase):
-    pass
+
+    def original_csv_file(self):
+        return StringIO("""bk, no, betaka, authno, cat
+        nama, 1, ini buku bagus, 1, 3
+        munawir, 2, kamus indo arab, 2, 4
+        sidu, 3, kertas putih, 4, 3""")
+
+    
