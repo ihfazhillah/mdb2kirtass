29ebf87ead16cff6ef74cb1993e55cda04e118fa
ihfazhillah <github.com>
2016-08-16 20:37:26 +0700

    menambah if col_name is None di bagian ketika parnet is not none

--- mdb2kirtass/csv_to_xml.py
+++ mdb2kirtass/csv_to_xml.py
@@ -17,8 +17,9 @@ class CsvtoXml(object):
         """Fungsi untuk mengubah file csv ke objek DictCsvReader"""
         return csv.DictReader(self.csv_file)
 
-    def _make_xml_original(self, root='item', parent=None):
+    def _make_xml_original(self, root='item', parent=None, col_name=None):
         item = etree.Element(root)
+        # if col_name is None:
         for row in self._dict_csv():
             if not parent:
                 for col in row:
@@ -26,8 +27,18 @@ class CsvtoXml(object):
                     t.text = row[col]
             else:
                 p = etree.SubElement(item, parent)
-                for col in row:
-                    t = etree.SubElement(p, col)
-                    t.text = row[col]
+                if col_name is None:
+                    for col in row:
+                        t = etree.SubElement(p, col)
+                        t.text = row[col]
+                else:
+                    for col in row:
+                        for cn in col_name:
+                            if col == cn[0]:
+                                col = cn
+                            else:
+                                col = (col, col)
+                        t = etree.SubElement(p, col[1])
+                        t.text = row[col[0]]
 
         return item
