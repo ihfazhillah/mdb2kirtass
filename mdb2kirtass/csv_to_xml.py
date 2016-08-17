36c18961e62627fc576766d625886ac73e7d0a0d
ihfazhillah <github.com>
2016-08-17 18:50:23 +0700

    menghapus _make_tag_and_rename_it blablabla

--- mdb2kirtass/csv_to_xml.py
+++ mdb2kirtass/csv_to_xml.py
@@ -23,20 +23,14 @@ class CsvtoXml(object):
         for row in self._dict_csv():
             if include:
                 row = self._process_include(row, include)
-            if tag is None:
+
+            if tag is None and as_attrib is False:
                 if parent is None:
-                    if col_name is None:
-                        self._make_tag_with_parent_from_row(row, item)
-                    else:
-                        self._make_tag_and_rename_it_inside_parent_from_row(row,
-                                                                     item, col_name)
+                    self._make_tag_with_parent_from_row(row, item, col_name)
                 else:
                     p = etree.SubElement(item, parent)
-                    if col_name is None:
-                        self._make_tag_with_parent_from_row(row, p)
-                    else:
-                        self._make_tag_and_rename_it_inside_parent_from_row(row,
-                                                        p, col_name)
+                    self._make_tag_with_parent_from_row(row, p, col_name)
+
             else:
                 if col_name is None:
                     attrib = {k:row[k] for k in row}
@@ -48,19 +42,12 @@ class CsvtoXml(object):
 
         return item
 
-    def _make_tag_with_parent_from_row(self, row, parent, as_attrib=False):
+    def _make_tag_with_parent_from_row(self, row, parent, col_name,
+                                        as_attrib=False):
         """Membuat tag, didalam parent yang ditentukan oleh *parent*
         dari *row* csv, DictReader objek"""
-        for col in row:
-            t = etree.SubElement(parent, col)
-            t.text = row[col]
-
-    def _make_tag_and_rename_it_inside_parent_from_row(self, row,
-                                                       parent, col_name):
-        """membuat tag, dengan nama yang diubah, didalam row objek DictReader,
-        col_name adalah list/tuple di dalam list/atau tuple, index 0 adalah
-        asli, index 1 adalah setelah diubah"""
-        self._change_col_name(row, col_name)
+        if col_name:
+            self._change_col_name(row, col_name)
 
         for col in row:
             t = etree.SubElement(parent, col)
@@ -71,6 +58,8 @@ class CsvtoXml(object):
 
         for c in col_name:
             row[c[1]] = row.pop(c[0])
+
+        return row
 
     def _process_include(self, row, include):
         return {key:row[key] for key in row if key in include}
