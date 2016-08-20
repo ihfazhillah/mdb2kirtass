45c7c6a8faf4a494df531106d5671d6ccf1afee3
ihfazhillah <github.com>
2016-08-20 11:58:44 +0700

    menambahkan original group string fungsi

--- tests/test_make_group.py
+++ tests/test_make_group.py
@@ -1,3 +1,17 @@
 import unittests
 from lxml import etree
 from lxml_asserts.testcase import LxmlTestCaseMixin
+
+
+class MakeGroupTestCase(unittests.TestCase, LxmlTestCaseMixin):
+
+    def original_group_string(self):
+        return """
+        <setting>
+            <root Name='abc' id='1'>
+                <Item Name='def' id='1'>
+                    <bk />
+                </Item>
+            </root>
+        </setting>
+        """.strip()
