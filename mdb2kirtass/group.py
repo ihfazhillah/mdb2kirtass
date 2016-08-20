886b512d3c5420996f809855e0e44c6d615c3d3b
ihfazhillah <github.com>
2016-08-20 13:00:27 +0700

    menambhakan suffix bila ditemukan rootx

--- mdb2kirtass/group.py
+++ mdb2kirtass/group.py
@@ -7,7 +7,6 @@ class MakeGroup():
         root = tree.findall(".//root[@id='sb']")
         if len(root) >= 1:
             count = len(root)
-            print(count)
             id_ = 'sb%s' %(count)
             name = 'كتب الشاملة%s' %(count)
             etree.SubElement(tree, "root", {"Name": name, 'id': id_})
