f93f91bddbdfca0b71aaf7cf76faa23f692637a3
ihfazhillah <github.com>
2016-08-20 12:05:54 +0700

    menambahkan kutub_shamlea not found test dan akan menambahkan yang baru

--- tests/test_make_group.py
+++ tests/test_make_group.py
@@ -1,9 +1,9 @@
-import unittests
+import unittest
 from lxml import etree
 from lxml_asserts.testcase import LxmlTestCaseMixin
 
 
-class MakeGroupTestCase(unittests.TestCase, LxmlTestCaseMixin):
+class MakeGroupTestCase(unittest.TestCase, LxmlTestCaseMixin):
 
     def original_group_string(self):
         return """
@@ -19,4 +19,17 @@ class MakeGroupTestCase(unittests.TestCase, LxmlTestCaseMixin):
     def original_group_etree(self):
         return etree.Element(self.original_group_string())
 
-    
+    def test_kutub_shamela_not_found(self):
+        group = MakeGroup()
+        add_root  = group.add_root(tree=self.original_group_etree())
+        expected = """
+        <setting>
+            <root Name='abc' id='1'>
+                <Item Name='def' id='1'>
+                    <bk />
+                </Item>
+            </root>
+            <root Name='كتب الشاملة' id='sb' />
+        </setting>
+        """
+        self.assertXmlEqual(expected, add_root)
