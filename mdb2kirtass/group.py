d3c50bbcd6beaaecf06d95932ef7d267629ba20a
ihfazhillah <github.com>
2016-08-20 12:59:29 +0700

    menambahkan suffix nomor ketika root ditemukan (fixed)

--- mdb2kirtass/group.py
+++ mdb2kirtass/group.py
@@ -4,9 +4,13 @@ from lxml import etree
 class MakeGroup():
 
     def add_root(self, tree):
-        root = tree.find(".//root[@id='sb']")
-        if root:
-            pass
+        root = tree.findall(".//root[@id='sb']")
+        if len(root) >= 1:
+            count = len(root)
+            print(count)
+            id_ = 'sb%s' %(count)
+            name = 'كتب الشاملة%s' %(count)
+            etree.SubElement(tree, "root", {"Name": name, 'id': id_})
         else:
             etree.SubElement(tree, "root",
             {'Name': 'كتب الشاملة', 'id': 'sb'})
